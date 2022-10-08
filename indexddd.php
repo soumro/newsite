9926787b2de62b7f33ce5c68674524ed6af6128e
soumro <ashrafsoomro@gmail.com>
2022-10-08 09:58:09 +0500

    Initial commit

---
+++ indexddd.php
@@ -0,0 +1,42 @@
+
+
+
+<?php
+
+  echo "php rocks";
+  echo "<br/>";
+
+  echo 9;
+
+
+?>
+<!DOCTYPE html>
+<html>
+	<head>
+	<title><?php echo "Page Tile"; ?> </title>
+	</head>
+	<body>
+
+	<h1>This is a Heading</h1>
+	<p><?php echo "this is our paragraph"; ?></p>
+
+	</body>
+</html>
+
+
+<?php
+print "<br/>";
+
+  echo 9 + 8;
+  print "<br/>";
+
+    print "hello from print";
+    print "<br/>";
+
+    print 9-7;
+
+    print "<br/>";
+
+    echo "this Arg 1" , "hello from arg 2";
+
+   ?>
